2b4a7604d70892ddf8c17df06c5defc8cac3203b
vyshnavi4375-crypto <vyshnavi4375@gmail.com>
2025-12-23 23:07:26 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,14 @@
+class VariablesExample {
+    public static void main(String[] args) {
+        int age = 15;
+        float marks = 89.5f;
+        char grade = 'A';
+        String name = "Vyshnavi";
+
+        System.out.println(age);
+        System.out.println(marks);
+        System.out.println(grade);
+        System.out.println(name);
+    }
+}
+
